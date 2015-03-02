9aa6db145c3da5f227ea7188856bdd07ff0e9b34
Ian Ferreira dos Santos <ianfs1@Ians-MacBook-Pro.local>
2015-03-02 14:55:09 -0300

    Initial Commit

---
+++ DesafioNSArrayClasse/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DesafioNSArrayClasse
+//
+//  Created by Ian Ferreira dos Santos on 3/2/15.
+//  Copyright (c) 2015 Ian Ferreira dos Santos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
